8a72dd45b1e9fddc6aa3e66df8b60ab3e7779153
Hanamantgit7259 <hanamant71198@gmail.com>
2025-08-04 01:36:51 +0530

    new commit after docker yaml update

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -16,9 +16,11 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -43,57 +45,48 @@ public class BaseClass {
 
         logger = LogManager.getLogger(this.getClass());
 
+        // Remote Execution (Selenium Grid in Docker)
         if (p.getProperty("execution_env").equalsIgnoreCase("remote")) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
+            URI hubUri = new URI("http://selenium-hub:4444/wd/hub");  // inside docker network
 
-            // OS
-            if (os.equalsIgnoreCase("windows")) {
-                capabilities.setPlatform(Platform.WIN11);
-            } else if (os.equalsIgnoreCase("linux")) {
-                capabilities.setPlatform(Platform.LINUX);
-            } else if (os.equalsIgnoreCase("mac")) {
-                capabilities.setPlatform(Platform.MAC);
-            } else {
-                System.out.println("No matching OS");
-                return;
-            }
-
-            // Browser
             switch (br.toLowerCase()) {
                 case "chrome":
-                    capabilities.setBrowserName("chrome");
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--disable-gpu");
+                    driver = new RemoteWebDriver(hubUri.toURL(), chromeOptions);
                     break;
-                case "edge":
-                    capabilities.setBrowserName("MicrosoftEdge");
-                    break;
-                case "firefox":
-                    capabilities.setBrowserName("firefox");
-                    break;
-                default:
-                    System.out.println("No matching browser");
-                    return;
-            }
 
-            // 👉 Use selenium-hub (Docker container name) instead of localhost
-            driver = new RemoteWebDriver(
-                    new URI("http://selenium-hub:4444/wd/hub").toURL(),
-                    capabilities);
+                case "firefox":
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    driver = new RemoteWebDriver(hubUri.toURL(), firefoxOptions);
+                    break;
+
+                case "edge":
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    driver = new RemoteWebDriver(hubUri.toURL(), edgeOptions);
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("No matching browser for remote execution: " + br);
+            }
         }
 
-        if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
+        // Local Execution
+        else if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
             switch (br.toLowerCase()) {
                 case "chrome":
                     driver = new ChromeDriver();
                     break;
-                case "edge":
-                    driver = new EdgeDriver();
-                    break;
                 case "firefox":
                     driver = new FirefoxDriver();
                     break;
+                case "edge":
+                    driver = new EdgeDriver();
+                    break;
                 default:
-                    System.out.println("Invalid browser name..");
-                    return;
+                    throw new IllegalArgumentException("Invalid local browser name: " + br);
             }
         }
 
