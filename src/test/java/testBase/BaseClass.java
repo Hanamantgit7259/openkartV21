7ac547b3a58b5ac7de4233badad17b03e486593c
Hanamantgit7259 <hanamant71198@gmail.com>
2025-08-06 00:16:28 +0530

    New_Updates

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,146 +3,143 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
+import java.util.Random;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 
-
-import org.apache.logging.log4j.LogManager;  //Log4j
-import org.apache.logging.log4j.Logger;  //Log4j
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BaseClass {
 
-public static WebDriver driver;
-public Logger logger;  //Log4j
-public Properties p;
-	
-	@BeforeClass(groups= {"Sanity","Regression","Master"})
-	@Parameters({"os","browser"})
-	public void setup(String os, String br) throws IOException
-	{
-		//Loading config.properties file
-		FileReader file=new FileReader("./src//test//resources//config.properties");
-		p=new Properties();
-		p.load(file);
-				
-		logger=LogManager.getLogger(this.getClass());  //lOG4J2
-				
-		if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
-		{
-			DesiredCapabilities capabilities=new DesiredCapabilities();
-			
-			//os
-			if(os.equalsIgnoreCase("windows"))
-			{
-				capabilities.setPlatform(Platform.WIN11);
-			}
-			else if(os.equalsIgnoreCase("linux"))
-			{
-				capabilities.setPlatform(Platform.LINUX);
-				
-			}
-			else if (os.equalsIgnoreCase("mac"))
-			{
-				capabilities.setPlatform(Platform.MAC);
-			}
-			else
-			{
-				System.out.println("No matching os");
-				return;
-			}
-			
-			//browser
-			switch(br.toLowerCase())
-			{
-			case "chrome": capabilities.setBrowserName("chrome"); break;
-			case "edge": capabilities.setBrowserName("MicrosoftEdge"); break;
-			case "firefox": capabilities.setBrowserName("firefox"); break;
-			default: System.out.println("No matching browser"); return;
-			}
-			
-			driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
-		}
-		
-				
-		if(p.getProperty("execution_env").equalsIgnoreCase("local"))
-		{
+    public static WebDriver driver;
+    public Logger logger;
+    public Properties p;
 
-			switch(br.toLowerCase())
-			{
-			case "chrome" : driver=new ChromeDriver(); break;
-			case "edge" : driver=new EdgeDriver(); break;
-			case "firefox": driver=new FirefoxDriver(); break;
-			default : System.out.println("Invalid browser name.."); return;
-			}
-		}
-		
-			
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		
-		driver.get(p.getProperty("appURL2")); // reading url from properties file.
-		driver.manage().window().maximize();
-	}
-	
-	@AfterClass(groups= {"Sanity","Regression","Master"})
-	public void tearDown()
-	{
-		driver.quit();
-	}
-	
-	public String randomeString()
-	{
-		String generatedstring=RandomStringUtils.randomAlphabetic(5);
-		return generatedstring;
-	}
-	
-	public String randomeNumber()
-	{
-		String generatednumber=RandomStringUtils.randomNumeric(10);
-		return generatednumber;
-	}
-	
-	public String randomeAlphaNumberic()
-	{
-		String generatedstring=RandomStringUtils.randomAlphabetic(3);
-		String generatednumber=RandomStringUtils.randomNumeric(3);
-		return (generatedstring+"@"+generatednumber);
-	}
-	
-	public String captureScreen(String tname) throws IOException {
+    @BeforeClass(groups= {"Sanity","Regression","Master"})
+    @Parameters({"os","browser"})
+    public void setup(String os, String br) throws IOException
+    {
+        // Load config.properties
+        FileReader file = new FileReader("./src//test//resources//config.properties");
+        p = new Properties();
+        p.load(file);
 
-		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-				
-		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
-		
-		String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
-		File targetFile=new File(targetFilePath);
-		
-		sourceFile.renameTo(targetFile);
-			
-		return targetFilePath;
+        logger = LogManager.getLogger(this.getClass());
 
-	}
-	
-	
+        String env = p.getProperty("execution_env");
+
+        if(env.equalsIgnoreCase("remote"))
+        {
+            // 🔹 Remote execution (Docker Grid, Jenkins)
+            if(br.equalsIgnoreCase("chrome")) {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
+             //   driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), options);
+
+                
+                
+            } else if(br.equalsIgnoreCase("firefox")) {
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+              //  driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), options);
+
+
+            } else if(br.equalsIgnoreCase("edge")) {
+                EdgeOptions options = new EdgeOptions();
+                options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
+              //  driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+                driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), options);
+
+
+            } else {
+                System.out.println("No matching browser found for remote execution.");
+                return;
+            }
+        }
+        else if(env.equalsIgnoreCase("local"))
+        {
+            // 🔹 Local execution (normal desktop browsers)
+            if(br.equalsIgnoreCase("chrome")) {
+                driver = new ChromeDriver();
+            } else if(br.equalsIgnoreCase("firefox")) {
+                driver = new FirefoxDriver();
+            } else if(br.equalsIgnoreCase("edge")) {
+                driver = new EdgeDriver();
+            } else {
+                System.out.println("Invalid browser name for local execution.");
+                return;
+            }
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(p.getProperty("appURL2")); // URL from config.properties
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass(groups= {"Sanity","Regression","Master"})
+    public void tearDown()
+    {
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+
+    // ✅ Random generators using Java's Random (no deprecation warnings)
+    public String randomeString() {
+        int leftLimit = 97; // 'a'
+        int rightLimit = 122; // 'z'
+        int length = 5;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                     .limit(length)
+                     .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                     .toString();
+    }
+
+    public String randomeNumber() {
+        Random random = new Random();
+        long number = (long)(1000000000L + random.nextDouble() * 9000000000L); // 10 digits
+        return String.valueOf(number);
+    }
+
+    public String randomeAlphaNumberic() {
+        return randomeString().substring(0, 3) + "@" + randomeNumber().substring(0, 3);
+    }
+
+    // Screenshot utility
+    public String captureScreen(String tname) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
+        File targetFile = new File(targetFilePath);
+
+        sourceFile.renameTo(targetFile);
+
+        return targetFilePath;
+    }
 }
